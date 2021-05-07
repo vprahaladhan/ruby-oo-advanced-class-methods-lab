40a664a54caea891ad3ad37f21baca6da0c7362c
prahal <vprahaladhan@gmail.com>
2021-05-07 21:03:01 +1000

    commit tree 6daf748bdc10854b16eeb22ca4055c933efbbbfb

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,47 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.create_by_name(name)
+    self.new_by_name(name)
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort{|song_a, song_b| song_a.name <=> song_b.name}
+  end
+
+  def self.new_from_filename(filename)
+    artist_name, name = filename.split(" - ")
+    song = self.new_by_name(name.split(".")[0])
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename)
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
 end
